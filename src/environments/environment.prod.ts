c7ad0a97a8b9c0f4922d79191d9e652b6980f222
amirzarafshani <amirzarafshani@gmail.com>
2022-11-26 22:14:29 +0300

    change environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'http://192.168.1.28:4000',
+  API_URL: 'http://212.253.87.46:4000',
+  // API_URL: 'http://192.168.1.28:4000',
   DEPOSIT_WALLET_ADDRESS: 'TC3HwYZ21kyFPyLJ6aSvYexvuYPm9cdugk',
 };
